3e2db26304b67c103b52cef6c4355a3e4ac4b583
i.am.goutham <97418751+GouthamSER@users.noreply.github.com>
2022-12-10 19:44:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001729830342'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'rai_info17')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'im_goutham_josh')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', 'True')), False)
 IMDB = is_enabled((environ.get('IMDB', 'True')), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', 'True')), False)
